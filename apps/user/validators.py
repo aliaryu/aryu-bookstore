db0cc125b00160ff143ae0045f387dbd394318c7
aliaryu <aliaryu@yahoo.com>
2024-01-17 09:13:14 +0330

    feat(app user/validators.py) : add validator birth date
    birth date cannot be in the future

--- apps/user/validators.py
+++ apps/user/validators.py
@@ -1,13 +1,21 @@
 from django.core.validators import RegexValidator
+from django.core.validators import MaxValueValidator
+from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from datetime import date
 
 
 phone_numeric_validator = RegexValidator(
-    regex   = r"^\d+$",
+    regex = r"^\d+$",
     message = _("phone number can only contain numbers")
 )
 
 phone_format_validator = RegexValidator(
-    regex   = r"^09\d{9}$",
+    regex = r"^09\d{9}$",
     message = _("phone number must start with 09 and it must be 11 digits")
+)
+
+birth_date_validator = MaxValueValidator(
+    limit_value = date.today(),
+    message = _("birth date cannot be in the future")
 )
