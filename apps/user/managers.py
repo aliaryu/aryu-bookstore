64b31284f29e2d888db1167832553f8d916b1d6b
aliaryu <aliaryu@yahoo.com>
2024-01-17 08:34:58 +0330

    feat(app user/manager.py) : add custom manager user
    with overrided get queryset method, archive and deleted methods, is active True for create superuser

---
+++ apps/user/managers.py
@@ -0,0 +1,19 @@
+from typing import Any
+from django.contrib.auth.models import UserManager
+from apps.core.managers import LogicalQuerySet
+
+
+class CustomUserManager(UserManager):
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault("is_active", True)
+        return super().create_superuser(username, email, password, **extra_fields)
+
+    def get_queryset(self):
+        return LogicalQuerySet(self.model, using=self._db).filter(is_delete=False)
+
+    def archive(self):
+        return LogicalQuerySet(self.model, using=self._db)
+
+    def deleted(self):
+        return LogicalQuerySet(self.model, using=self._db).filter(is_delete=True)
