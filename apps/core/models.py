6bb2c075efe1779e5733e9c79dd617fd9e72c97b
aliaryu <aliaryu@yahoo.com>
2024-01-20 03:33:30 +0330

    feat(app core/models) : add logical base model
    basic model for models that implement logical deletion
    basic model for models that hav

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,1 +1,26 @@
 from django.db import models
+from .managers import LogicalManager
+from django.utils.translation import gettext_lazy as _
+
+
+class LogicalBaseModel(models.Model):
+    is_delete = models.BooleanField(
+        verbose_name = _("delete"),
+        default = False
+    )
+
+    objects = LogicalManager()
+
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_delete = True
+        self.save()
+
+    def undelete(self):
+        self.is_delete = False
+        self.save()
+
+    def force_delete(self):
+        super().delete()
