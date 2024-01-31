610ca22e457539fa2ed910413f8254646c0d7148
aliaryu <aliaryu@yahoo.com>
2024-01-31 19:21:20 +0330

    feat(app product/models) : add category model
    this model have self relation for sub categories

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    cat_name = models.CharField(
+        verbose_name = _("category"),
+        max_length = 255,
+        unique = True,
+    )
+    cat_parent = models.ForeignKey(
+        verbose_name = _("parent"),
+        to = "self",
+        on_delete = models.CASCADE
+    )
+    image = models.ImageField(
+        verbose_name = _("image"),
+        upload_to = "category_image/",
+        blank = True,
+        null = True,
+    )
+
+    class Meta:
+        verbose_name = _("category")
+        verbose_name_plural = _("categories")
+
+    def __str__(self):
+        return f"{self.cat_name}"
