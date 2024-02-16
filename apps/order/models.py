cff72d7b0f025cf6b0fad9dacb56e6d5884ba8b2
aliaryu <aliaryu@yahoo.com>
2024-02-16 15:49:43 +0330

    feat(app order/model orderbook) : add model orderbook
    its a middle model between order and book models, include count field and overriden methods

--- apps/order/models.py
+++ apps/order/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from apps.core.models import LogicalBaseModel, TimeStampBaseModel
+from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 
 
@@ -51,3 +52,36 @@ class Order(LogicalBaseModel, TimeStampBaseModel):
     def __str__(self):
         return f"order number: [ {self.id} ]"
 
+
+class OrderBook(LogicalBaseModel):
+    order = models.ForeignKey(
+        verbose_name = _("order"),
+        to = "Order",
+        on_delete = models.CASCADE,
+    )
+    book = models.ForeignKey(
+        verbose_name = _("book"),
+        to = "product.Book",
+        on_delete = models.CASCADE,
+    )
+    count = models.PositiveIntegerField(
+        verbose_name = _("count"),
+    )
+
+    def clean(self):
+        super().clean()
+        if self.count > self.book.count:
+            raise ValidationError(_("The order quantity is more than the available stock."))
+        
+    def save(self, *args, **kwargs):
+        updated_count = self.book.count - self.count
+        self.book.count = updated_count
+        self.book.save()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = _("orderd book")
+        verbose_name_plural = _("ordered books")
+
+    def __str__(self):
+        return f"item id: [ {self.item.id} ] - order id: [ {self.order.id} ]"
