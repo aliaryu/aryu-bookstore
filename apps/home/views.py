c5c60a02304ddb1e3a24e196154f2d63b0e7bc01
aliaryu <aliaryu@yahoo.com>
2024-03-08 19:51:32 +0330

    feat(app home/home view) : add popular books context
    needs performance update, select related with discount

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from apps.product.models import Book
+from django.db.models import Count
 
 
 class HomeView(TemplateView):
@@ -7,5 +9,10 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        context["popular_books"] = Book.objects.annotate(likes_count=Count('likes')).order_by("-likes_count", "-id")[:10]
+
+        print(context["popular_books"])
+
 
         return context
