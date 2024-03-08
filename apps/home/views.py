cf7c4bc1c6f09cd964f50868086eb89f8a4b605d
aliaryu <aliaryu@yahoo.com>
2024-03-08 22:00:53 +0330

    feat(app home/home view) : add authors context

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from apps.product.models import Book
+from apps.product.models import Book, Author, Tag
 from django.db.models import Count
 
 
@@ -11,8 +11,6 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
 
         context["popular_books"] = Book.objects.select_related("discount").annotate(likes_count=Count('likes')).order_by("-likes_count", "-id")[:10]
-
-        print(context["popular_books"])
-
+        context["authors"] = Author.objects.order_by("-id")[:50]
 
         return context
