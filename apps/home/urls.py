45fa62772699014ffd68d9410763a230ea2a8d52
aliaryu <aliaryu@yahoo.com>
2024-03-09 18:03:59 +0330

    feat(app home/urls) : create path for contact form api

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from . import views
+from .views import HomeView
+from .api.views import ContactCreateAPIView
 
 
 app_name = "home"
 
 urlpatterns = [
-    path("", views.HomeView.as_view(), name="home"),
+    path("", HomeView.as_view(), name="home"),
+    path("contact/",ContactCreateAPIView.as_view(), name="contact")
 ]
