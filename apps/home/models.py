501a34a21e86347dd87625320c612ffcfa800b57
aliaryu <aliaryu@yahoo.com>
2024-03-09 04:31:15 +0330

    feat(app home/models) : add contact model
    needs add it to ERD later, needs complations (form, serializer, js)

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from apps.core.models import TimeStampBaseModel
 
-# Create your models here.
+
+class Contact(TimeStampBaseModel):
+    full_name = models.CharField(
+        verbose_name = _("full name"),
+        max_length = 255,
+    )
+    email = models.EmailField(
+        verbose_name = _("email"),
+        max_length = 255,
+    )
+    text = models.TextField(
+        verbose_name = _("text"),
+    )
+    read = models.BooleanField(
+        verbose_name = _("read"),
+        default = False,
+    )
+
+    class Meta:
+        verbose_name = _("message")
+        verbose_name_plural = _("messages")
+
+    def __str__(self):
+        return f"{self.email} - {self.text[:10]}"
