6df7a29fd7ac70043c427a7a94cab4a467015985
aliaryu <aliaryu@yahoo.com>
2024-03-10 22:23:15 +0330

    feat(app user/views) : add login view

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class LoginView(TemplateView):
+    template_name = "user/login.html"
