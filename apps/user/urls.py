e26c4e0787326e8cf9d79da860391a5221499d99
aliaryu <aliaryu@yahoo.com>
2024-03-12 21:30:18 +0330

    feat(app user/urls) : add userpass url, use user pass login view

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import LoginView
+from .api.views import UserPassLoginView
 
 
 app_name = "user"
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), name="login")
+    path("login/", LoginView.as_view(), name="login"),
+    path("userpass/", UserPassLoginView.as_view(), name="userpass"),
 ]
