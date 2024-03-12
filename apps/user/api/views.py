32a1d8b48981b6bd7602efefbabe6700d8498666
aliaryu <aliaryu@yahoo.com>
2024-03-12 22:01:31 +0330

    feat(app user/api, views) : add user pass login view

---
+++ apps/user/api/views.py
@@ -0,0 +1,26 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_401_UNAUTHORIZED
+)
+from .serializers import UserPassLoginSerializer
+from django.contrib.auth import login
+from django.utils.translation import gettext_lazy as _
+
+
+class UserPassLoginView(APIView):
+    serializer_class = UserPassLoginSerializer
+
+    def post(self, request):
+        print(request.data, "*" * 10)
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        
+        if user:
+            # login(request, user)
+            return Response({"message": _("login successfull.")}, status=HTTP_200_OK)
+        else:
+            return Response({"message": _("invalid username or password.")}, status=HTTP_401_UNAUTHORIZED)
+
