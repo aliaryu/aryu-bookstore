19321502c534f10afb2bd595c0fef5f4a699ceef
aliaryu <aliaryu@yahoo.com>
2024-03-13 03:06:39 +0330

    feat(app user/login view) : add next page parameter to context

--- apps/user/views.py
+++ apps/user/views.py
@@ -6,6 +6,12 @@ from django.shortcuts import redirect
 class LoginView(UserPassesTestMixin, TemplateView):
     template_name = "user/login.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if (next:=self.request.GET.get("next", None)):
+            context["next"] = next
+        return context
+
     def test_func(self):
         return not self.request.user.is_authenticated
 
