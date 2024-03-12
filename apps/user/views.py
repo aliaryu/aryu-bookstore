4c4ca6c339cc63653739dfcf1bf8dd18962b056d
aliaryu <aliaryu@yahoo.com>
2024-03-12 18:04:02 +0330

    feat(app user/login view) : add user passes test mixin to login view
    it checks if user is authenticated, redirected it to home page

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,5 +1,13 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import redirect
 
 
-class LoginView(TemplateView):
+class LoginView(UserPassesTestMixin, TemplateView):
     template_name = "user/login.html"
+
+    def test_func(self):
+        return not self.request.user.is_authenticated
+
+    def handle_no_permission(self):
+        return redirect('home:home')
