9689a9b40d782bc5b700ab3d6d5bf175ca5e21b7
aliaryu <aliaryu@yahoo.com>
2024-03-13 05:21:44 +0330

    feat(app user/urls) : add logout url
    also use django auth logout class as view for logout

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import LoginView
 from .api.views import UserPassLoginView
+from django.contrib.auth.views import LogoutView
 
 
 app_name = "user"
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("userpass/", UserPassLoginView.as_view(), name="userpass"),
+
 ]
