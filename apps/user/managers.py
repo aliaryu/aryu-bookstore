d0e3994c39f1bfdabc92847044ee58914c0d7300
aliaryu <aliaryu@yahoo.com>
2024-03-14 22:57:04 +0330

    feat(app user/manager, custom user manager) : change create super user method

--- apps/user/managers.py
+++ apps/user/managers.py
@@ -1,12 +1,27 @@
+from typing import Any
 from django.contrib.auth.models import UserManager
 from apps.core.managers import LogicalQuerySet, LogicalManager
 from django.db import models
 
 
 class CustomUserManager(UserManager, LogicalManager):
-    def create_superuser(self, username, email, password, **extra_fields):
+    def create_user(self, username, email, password, **extra_fields):
+
+        
+        return super().create_user(username, email, password, **extra_fields)
+
+    
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
-        return super().create_superuser(username, email, password, **extra_fields)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(username, email, password, **extra_fields)
 
 
 class UserRelatedModelBaseManager(models.Manager):
