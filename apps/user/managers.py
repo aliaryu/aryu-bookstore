8dd7969acf4ca79b18f004e9ab492ec2af52e7b4
aliaryu <aliaryu@yahoo.com>
2024-03-14 23:24:56 +0330

    feat(app user/managers, custom user manager) : add create user method to custom user manager
    it checks if debug is true, the user becomes active, otherwise its active false

--- apps/user/managers.py
+++ apps/user/managers.py
@@ -1,16 +1,25 @@
 from typing import Any
 from django.contrib.auth.models import UserManager
-from apps.core.managers import LogicalQuerySet, LogicalManager
+from apps.core.managers import LogicalManager
 from django.db import models
+from config import settings
+
 
 
 class CustomUserManager(UserManager, LogicalManager):
     def create_user(self, username, email, password, **extra_fields):
+        if not username:
+            raise ValueError("The given username must be set")
+        if settings.DEBUG:
+            extra_fields.setdefault("is_active", True)
 
-        
-        return super().create_user(username, email, password, **extra_fields)
+        username = self.model.normalize_username(username)
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-    
     def create_superuser(self, username, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
