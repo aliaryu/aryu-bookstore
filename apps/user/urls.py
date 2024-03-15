d3ba50a7e1b4fb595464eafd124830be35098c55
aliaryu <aliaryu@yahoo.com>
2024-03-15 21:02:24 +0330

    feat(app user/urls) : add signup url

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, SignUpView
 from .api.views import UserPassLoginView
 from django.contrib.auth.views import LogoutView
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("userpass/", UserPassLoginView.as_view(), name="userpass"),
+    path("signup/", SignUpView.as_view(), name="signup")
 
 ]
