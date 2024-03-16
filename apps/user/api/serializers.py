4377870ac91a5ea6d05534f214b3897b154b7a22
aliaryu <aliaryu@yahoo.com>
2024-03-16 08:08:42 +0330

    feat(app user/api, serializer) : add user sign up serializer
    it checks passwords, validate informations, create user

--- apps/user/api/serializers.py
+++ apps/user/api/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError as DjangoValidationError
+
+
+User = get_user_model()
 
 
 class UserPassLoginSerializer(serializers.Serializer):
@@ -13,3 +19,34 @@ class UserPassLoginSerializer(serializers.Serializer):
         user = authenticate(username=username, password=password)
         data["user"] = user
         return data
+    
+
+class UserSignUpSerializer(serializers.ModelSerializer):
+    password_confirm = serializers.CharField(max_length=128, allow_blank=False, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'password', 'password_confirm', 'email']
+        extra_kwargs = {
+            'first_name': {'required': True, "allow_blank": False},
+            'last_name': {'required': True, "allow_blank": False}
+        }
+
+    def validate_password(self, value):
+        try:
+            validate_password(value)
+        except DjangoValidationError as e:
+            raise serializers.ValidationError(e.messages)
+        return value
+
+    def validate(self, data):
+        if data.get('password') != data.get('password_confirm'):
+            raise serializers.ValidationError(_("passwords are not same."))
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(
+            **validated_data
+        )
+        return user
