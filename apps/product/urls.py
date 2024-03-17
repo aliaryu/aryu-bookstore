0ad67d34bf2d91a43bf09b4d3ba43bd9ed95723d
aliaryu <aliaryu@yahoo.com>
2024-03-17 18:05:05 +0330

    feat(app product/urls) : add comment url

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import BookDetailView
-from .api.views import LikeUnlikeBookAPIView, SaveUnsaveBookAPIView
+from .api.views import (
+    LikeUnlikeBookAPIView,
+    SaveUnsaveBookAPIView,
+    CommentBookAPIView
+)
 
 
 app_name = "product"
@@ -9,4 +13,5 @@ urlpatterns = [
     path("book/<int:pk>", BookDetailView.as_view(), name="book"),
     path("book/<int:pk>/likeunlike", LikeUnlikeBookAPIView.as_view(), name="likeunlike"),
     path("book/<int:pk>/saveunsave", SaveUnsaveBookAPIView.as_view(), name="saveunsave"),
+    path("book/<int:pk>/comment", CommentBookAPIView.as_view(), name="bookcomment"),
 ]
