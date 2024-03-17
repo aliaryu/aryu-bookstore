480c93d9bdee24d2a0aa645a43cc3cf8716b589c
aliaryu <aliaryu@yahoo.com>
2024-03-17 10:00:52 +0330

    feat(app product/views) : add book detail view
    needs completions, include select relateds & prefetchs, improve performance

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from apps.product.models import Book
 
-# Create your views here.
+
+class BookDetailView(DetailView):
+    template_name = "product/book.html"
+    context_object_name = "book"
+    queryset = Book.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
