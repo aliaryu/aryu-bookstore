ad9633fcc97824fdf98b25cad1baafd7bde473e5
aliaryu <aliaryu@yahoo.com>
2024-03-17 18:04:23 +0330

    feat(app product/api, views) : add comment book view

--- apps/product/api/views.py
+++ apps/product/api/views.py
@@ -2,7 +2,9 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from apps.product.models import Book
+from apps.comment.models import Comment
 from rest_framework.permissions import IsAuthenticated
+from .serializers import CommentSerializer
 from django.utils.translation import gettext_lazy as _
 
 
@@ -28,7 +30,7 @@ class LikeUnlikeBookAPIView(APIView):
             
         except Book.DoesNotExist:
             return Response({"error": _("book not found.")}, status=status.HTTP_404_NOT_FOUND)
-        
+
 
 class SaveUnsaveBookAPIView(APIView):
     permission_classes = [IsAuthenticated]
@@ -50,5 +52,24 @@ class SaveUnsaveBookAPIView(APIView):
             elif action == "save":
                 return Response({"save": True}, status=status.HTTP_200_OK)
             
+        except Book.DoesNotExist:
+            return Response({"error": _("book not found.")}, status=status.HTTP_404_NOT_FOUND)
+
+
+class CommentBookAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = CommentSerializer
+
+    def post(self, request, pk):
+        try:
+            serializer = self.serializer_class(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            book = Book.objects.get(pk=pk)
+            Comment.objects.create(
+                user = request.user,
+                text = serializer.validated_data['message'],
+                content_object = book
+            )
+            return Response({"comment": True}, status=status.HTTP_200_OK)
         except Book.DoesNotExist:
             return Response({"error": _("book not found.")}, status=status.HTTP_404_NOT_FOUND)
