290d21564071735c74bafd0a239d2b7fbe75a935
aliaryu <aliaryu@yahoo.com>
2024-03-17 16:25:34 +0330

    feat(app product/urls) : add save & unsave book url

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import BookDetailView
-from .api.views import LikeUnlikeBookAPIView
+from .api.views import LikeUnlikeBookAPIView, SaveUnsaveBookAPIView
 
 
 app_name = "product"
@@ -8,4 +8,5 @@ app_name = "product"
 urlpatterns = [
     path("book/<int:pk>", BookDetailView.as_view(), name="book"),
     path("book/<int:pk>/likeunlike", LikeUnlikeBookAPIView.as_view(), name="likeunlike"),
+    path("book/<int:pk>/saveunsave", SaveUnsaveBookAPIView.as_view(), name="saveunsave"),
 ]
