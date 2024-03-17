691cd326f8ded3071ec57d46297032abc61f88d2
aliaryu <aliaryu@yahoo.com>
2024-03-17 12:05:24 +0330

    perf(app product/views, book detail) : improve performance query
    with select related and prefetch related methods, needs more improving

--- apps/product/views.py
+++ apps/product/views.py
@@ -5,7 +5,18 @@ from apps.product.models import Book
 class BookDetailView(DetailView):
     template_name = "product/book.html"
     context_object_name = "book"
-    queryset = Book.objects.all()
+
+    # NEEDS DEFFER & ONLY
+    queryset = Book.objects.select_related(
+        "category",
+        "discount"
+    ).prefetch_related(
+        "tag",
+        "genre",
+        "author",
+        "translator",
+        "comments__user"
+    )
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
