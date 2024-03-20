0189f1a82eac4d42cb26efe1464a4be5a286cd12
aliaryu <aliaryu@yahoo.com>
2024-03-20 13:35:30 +0330

    feat(app product/context processors) : add categories context processor
    also add it into settings

--- config/settings.py
+++ config/settings.py
@@ -60,6 +60,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                # CUSTOM CONtEXT PROCESSORS
+                "apps.product.context_processors.categories"
             ],
         },
     },
