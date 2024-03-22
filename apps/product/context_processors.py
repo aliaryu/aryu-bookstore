b6dac7883e507a9c9bbc2eca48073750cd454145
aliaryu <aliaryu@yahoo.com>
2024-03-22 11:52:01 +0330

    perf(app product/context processor categories) : add prefetch related to self
    it improve queryset and return all categories with its sub categories

--- apps/product/context_processors.py
+++ apps/product/context_processors.py
@@ -2,5 +2,5 @@ from .models import Category
 
 
 def categories(request):
-    categories = Category.objects.filter(cat_parent__isnull=True)
+    categories = Category.objects.filter(cat_parent__isnull=True).prefetch_related("category_set")
     return {"categories": categories}
