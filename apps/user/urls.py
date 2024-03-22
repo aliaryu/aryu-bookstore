74962a9741c585b7c9df312701a39158e8a1d14b
aliaryu <aliaryu@yahoo.com>
2024-03-22 12:24:47 +0330

    feat(app user/urls) : add profile url

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,15 +1,26 @@
 from django.urls import path
-from .views import LoginView, SignUpView
-from .api.views import UserPassLoginView, UserSignUpView
+from .views import (
+    LoginView,
+    SignUpView,
+    ProfileView
+)
+from .api.views import (
+    UserPassLoginView,
+    UserSignUpView,
+)
 from django.contrib.auth.views import LogoutView
 
 
 app_name = "user"
 
 urlpatterns = [
+    # LOGIN & LOGOUT & SIGNUP
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("userpass/", UserPassLoginView.as_view(), name="userpass"),
     path("signup/", SignUpView.as_view(), name="signup"),
     path("usersignup/", UserSignUpView.as_view(), name="usersignup"),
+
+    # PROFILE
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
