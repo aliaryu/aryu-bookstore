e81285f974721c6daf048b5284b2b44317164ff7
aliaryu <aliaryu@yahoo.com>
2024-03-23 02:42:12 +0330

    feat(app user/forms) : add user address form

--- apps/user/forms.py
+++ apps/user/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from apps.user.models import Address
 from django.contrib.auth import get_user_model
 
 
@@ -36,4 +37,32 @@ class UserInformationUpdateForm(forms.ModelForm):
             "class": "form-control bg-transparent place-holder-grey fw-bold me-1",
             "placeholder": "ایمیل",
             "type": "email",
+        })
+
+
+class UserAddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['province'].choices = list(self.fields['province'].choices)[1:]
+        self.fields["province"].widget.attrs.update({
+           "class": "form-select ms-1",
+           "id": "inputGroupSelect01",
+           "required": True
+        })
+        self.fields["province"].empty_label = "انتخاب کنید"
+        self.fields["postal_code"].widget.attrs.update({
+           "class": "form-control bg-transparent place-holder-grey fw-bold me-1",
+           "placeholder": "کد پستی",
+           "type": "text",
+           "required": True
+        })
+        self.fields["address_path"].widget.attrs.update({
+           "class": "form-control bg-transparent place-holder-grey fw-bold",
+           "placeholder": "آدرس ...",
+           "rows": "3",
+           "required": True
         })
