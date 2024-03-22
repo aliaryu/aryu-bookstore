864166a64c8ad594a2f92efafb736e506146c5bd
aliaryu <aliaryu@yahoo.com>
2024-03-22 12:58:45 +0330

    feat(app user/api, views) : add upload user image view

--- apps/user/api/views.py
+++ apps/user/api/views.py
@@ -2,13 +2,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import (
     HTTP_200_OK,
-    HTTP_401_UNAUTHORIZED
+    HTTP_401_UNAUTHORIZED,
+    HTTP_400_BAD_REQUEST
 )
-from .serializers import UserPassLoginSerializer
 from django.contrib.auth import login
 from django.utils.translation import gettext_lazy as _
 from rest_framework.generics import CreateAPIView
-from .serializers import UserSignUpSerializer
+from .serializers import (
+    UserPassLoginSerializer,
+    UserSignUpSerializer,
+    UserImageSerializer
+)
 
 
 class UserPassLoginView(APIView):
@@ -28,3 +32,13 @@ class UserPassLoginView(APIView):
 
 class UserSignUpView(CreateAPIView):
     serializer_class = UserSignUpSerializer
+
+
+class UploadUserImageView(APIView):
+    def put(self, request, *args, **kwargs):
+        user = request.user
+        serializer = UserImageSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": True}, status=HTTP_200_OK)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
