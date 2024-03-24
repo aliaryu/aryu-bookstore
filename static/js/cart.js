112ec9059e25da32b243b1c05761bf7050c44e15
aliaryu <aliaryu@yahoo.com>
2024-03-24 13:41:10 +0330

    feat(static/js, cart) : add cart info function & call it on load dom
    it calculate cart information(count & price), also call it when full delete item function called

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,4 +1,26 @@
+book_items = document.getElementsByClassName("book-item");
+count_items = document.getElementById("count-items");
+price_items = document.getElementById("price-items");
+
+function cart_info() {
+    var default_count_items = 0;
+    var default_price_items = 0;
+    Array.from(book_items).forEach(item => {
+        var price_item = item.getAttribute("price");
+        var count_item = item.getAttribute("count");
+        default_count_items += parseInt(count_item);
+        default_price_items += parseFloat(price_item) * parseInt(count_item);
+    });
+    count_items.textContent = default_count_items;
+    price_items.textContent = default_price_items.toLocaleString('us-EN') + " تومان";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    cart_info();
+});
+
 function full_delete_item(book_id, event) {
     delete_item(book_id);
-    event.target.parentNode.remove()
+    event.target.parentNode.remove();
+    cart_info();
 }
