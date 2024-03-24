c82617893d9c253d7bf13832731e70856140ca69
aliaryu <aliaryu@yahoo.com>
2024-03-24 22:22:34 +0330

    feat(app order/cart view) : add payment form context

--- apps/order/views.py
+++ apps/order/views.py
@@ -1,10 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 from apps.product.models import Book
+from .forms import PaymentForm
 import json
 
 
 class CartView(TemplateView):
     template_name = "order/cart.html"
+    form_class = PaymentForm
 
     def get_cookie_cart(self):
         if (cart:=self.request.COOKIES.get("cart")):
@@ -13,8 +15,8 @@ class CartView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["payment_form"] = self.form_class(self.request.user)
         context["books"] = []
-
         cart = self.get_cookie_cart()
         if cart:
             books = Book.objects.filter(id__in=cart.keys()).select_related("discount").prefetch_related("author")
