4c548dd94ecf5c45e7378356b3c279dbef633edc
aliaryu <aliaryu@yahoo.com>
2024-03-24 12:43:50 +0330

    feat(app order/cart view) : add get cookie method & context books
    get cookie method returns a dictionary pair of key(id):value(count) or empty dict

--- apps/order/views.py
+++ apps/order/views.py
@@ -1,10 +1,24 @@
 from django.views.generic import TemplateView
+from apps.product.models import Book
+import json
 
 
 class CartView(TemplateView):
     template_name = "order/cart.html"
 
+    def get_cookie_cart(self):
+        if (cart:=self.request.COOKIES.get("cart")):
+            return json.loads(cart)
+        return {}
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["books"] = []
+
+        cart = self.get_cookie_cart()
+        if cart:
+            books = Book.objects.filter(id__in=cart.keys())
+            for book in books:
+                context["books"].append((book, cart[str(book.id)]))
 
         return context
