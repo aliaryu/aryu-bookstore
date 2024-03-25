e3d5178680716db24efbbbd6f56a9a8c0a721b38
aliaryu <aliaryu@yahoo.com>
2024-03-25 04:18:20 +0330

    feat(app order/view cart) : add post method
    handle form and create order

--- apps/order/views.py
+++ apps/order/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import TemplateView, FormView
+from django.views.generic import TemplateView
 from apps.product.models import Book
 from .forms import PaymentForm
+from django.shortcuts import render
 import json
 
 
@@ -24,3 +25,22 @@ class CartView(TemplateView):
                 context["books"].append((book, cart[str(book.id)]))
 
         return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(
+            user = request.user, 
+            cookie = self.get_cookie_cart(),
+            data = request.POST
+        )
+
+        context = self.get_context_data()
+
+        if form.is_valid():
+            context["payment_form"] = self.form_class(self.request.user)
+            context["payment_success"] = True
+            response = render(request, self.template_name, context)
+            response.delete_cookie("cart")
+            return response
+        else:
+            context['payment_form'] = form
+            return render(request, self.template_name, context)
